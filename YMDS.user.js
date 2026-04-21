fe78768203337865f74baf3e7025aef149b9c515
Noah <138242851+Meletion@users.noreply.github.com>
2026-04-21 07:39:37 -0700

    Update script version number, and also inputs into comments, or a search bar don

--- YMDS.user.js
+++ YMDS.user.js
@@ -1,22 +1,31 @@
 // ==UserScript==
 // @name         Music Downloader
 // @namespace    https://meletion.github.io/bio
-// @version      1
+// @version      1.0.3
 // @description  Download videos with ease
 // @author       Meletion
 // @match        https://www.youtube.com/watch?v=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
-
 (function () {
     'use strict';
-
+    function isTypingTarget(el) {
+    const tag = el.nodeName;
+    return (
+        tag === "INPUT" ||
+        tag === "TEXTAREA" ||
+        tag === "SELECT" ||
+        el.isContentEditable // catches <div contenteditable="true"> like YouTube's search
+      );
+    }
     document.addEventListener('keydown', function (e) {
         e = e || window.event;
-
-        // Shift + d
+        // Shift + D
         if (e.key == "D") {
+            if (isTypingTarget(document.activeElement)) {
+                return;
+            }
             fetch('http://localhost:3000/download', {
                 method: 'POST',
                 headers: {
@@ -24,9 +33,6 @@
                 },
                 body: new URLSearchParams(document.location.search).get('v')
             });
-
         }
-
-    }) 
-
+    });
 })();
